474e33c43d8bd04eaea25e237a92e32b74acf6f8
Paweł Turaczyk <kantal.streamy@gmail.com>
2018-10-14 21:16:21 +0200

    Paddle Moving
    Adds paddle moving and clamp position

---
+++ Assets/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15;
+
+    // Use this for initialization
+    void Start () {
+    //Set Cursor to not be visible
+         Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+    //paddle moving
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+    //limit of paddle position
+        paddlePosition.x = Mathf.Clamp(mousePositionInUnits, minX, maxX);
+        transform.position = paddlePosition;
+	}
+}
---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGameplay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadStart()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+    public void Next()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
+}
