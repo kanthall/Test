735997dc0cba297e88aceb7fd87317af705b897b
Paweł Turaczyk <kantal.streamy@gmail.com>
2018-10-27 00:28:01 +0200

    Particles for destroying blocks
    Adds particle effect for destroying blocks

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject effect;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Instantiate(effect, transform.position, Quaternion.identity);
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Object.Destroy(gameObject);
     }
 }
